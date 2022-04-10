0c76de86b12b0f56186bbab353b0e62adea1569f
Kari Marttila <kari.marttila@iki.fi>
2022-04-10 13:58:39 +0300

    Tooling for backend, frontend and css is ready, frontend starts properly in browser

--- src/cljs/simpleserver/main.cljs
+++ src/cljs/simpleserver/main.cljs
@@ -7,12 +7,73 @@
             [reitit.coercion.spec :as rss]
             [reitit.frontend :as rf]
             [reitit.frontend.easy :as rfe]
+            [reitit.frontend.controllers :as rfc]
             [simpleserver.util :as ss-util]
             [simpleserver.state :as ss-state]
             [simpleserver.view.home :as ssv-home]))
 
 
+
+;; ******************************************************************
+;; NOTE: When starting ClojureScript REPL in Cursive, give first command:
+; (shadow.cljs.devtools.api/repl :app)
+; to connect the REPL to the app running in the browser.
+;; ******************************************************************
+
+;;; Events ;;;
+
+(re-frame/reg-event-db
+  ::initialize-db
+  (fn [_ _]
+    {:current-route nil
+     :token nil
+     :debug true
+     :login nil
+     :signin nil}))
+
+(re-frame/reg-event-fx
+  ::ss-state/navigate
+  (fn [_ [_ & route]]
+    ;; See `navigate` effect in routes.cljs
+    {::navigate! route}))
+
+(re-frame/reg-event-db
+  ::ss-state/navigated
+  (fn [db [_ new-match]]
+    (let [old-match (:current-route db)
+          new-path (:path new-match)
+          controllers (rfc/apply-controllers (:controllers old-match) new-match)]
+      (js/console.log (str "new-path: " new-path))
+      (cond-> (assoc db :current-route (assoc new-match :controllers controllers))
+              (= "/" new-path) (-> (assoc :signin nil)
+                                   (assoc :login nil))))))
+
+(re-frame/reg-event-fx
+  ::ss-state/logout
+  (fn [cofx [_]]
+    {:db (assoc (:db cofx) :jwt nil)
+     :fx [[:dispatch [::ss-state/navigate ::ss-state/home]]]}))
+
+
+;; Effects
+
+;; Triggering navigation from events.
+(re-frame/reg-fx
+  ::navigate!
+  (fn [route]
+    (apply rfe/push-state route)))
+
+
 ;; Routes
+
+(defn href
+  "Return relative url for given route. Url can be used in HTML links."
+  ([k]
+   (href k nil nil))
+  ([k params]
+   (href k params nil))
+  ([k params query]
+   (rfe/href k params query)))
 
 (def routes
   ["/"
@@ -35,7 +96,7 @@
     {:data {:coercion rss/coercion}}))
 
 (defn init-routes! []
-  (ss-util/debug? (prn "ENTER init-routes!"))
+  (when ss-util/debug? (prn "ENTER init-routes!"))
   (rfe/start!
     router
     on-navigate
@@ -56,7 +117,7 @@
 ;; Setup
 
 (defn dev-setup []
-  (when (ss-util/debug?)
+  (when ss-util/debug?
     (enable-console-print!)
     (prn "dev mode")))
 
